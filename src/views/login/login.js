8d88e1a943618681de1caa8c9dbbbc0c76032393
dinghy <888888>
2024-04-17 21:49:51 +0800

    修改

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,5 +1,78 @@
-export default function Login(){
+import './login.css'
+import {Button, Form, Input} from 'antd';
+
+export default function Login() {
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <div>Login</div>
+        <div className='login'>
+            <div className={'content'}>
+                <h2>酒店后台管理系统</h2>
+                <Form
+                    name="basic"
+                    labelCol={{
+                        span: 4,
+                    }}
+                    wrapperCol={{
+                        span: 18,
+                    }}
+                    style={{
+                        maxWidth: 600,
+                    }}
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="账号"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入账号！',
+                            },
+                        ]}
+                    >
+                        <Input/>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="密码"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入密码!',
+                            },
+                        ]}
+                    >
+                        <Input.Password/>
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 4,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            登录
+                        </Button>
+                        <Button style={{marginLeft:'10px'}}>
+                            取消
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
     )
 }
